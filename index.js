c5eecc19fcb48dead875968e8e76854a0eb447ca
codeconcept <contact@codeconcept.fr>
2021-10-07 17:50:33 +0200

    Calendar configuration

---
+++ index.js
@@ -0,0 +1,22 @@
+let calendar = new VanillaCalendar({
+  selector: "#myCalendar",
+  months: [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ],
+  shortWeekday: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+  onSelect: (data, elem) => {
+      console.log(data);
+      console.log(elem);
+  },
+});
