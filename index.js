b89d1323a74c69badb3a0ff071856cd65e3793ac
codeconcept <contact@codeconcept.fr>
2021-10-07 18:42:17 +0200

    Form submission

--- index.js
+++ index.js
@@ -16,11 +16,24 @@ const calendar = new VanillaCalendar({
   ],
   shortWeekday: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
   onSelect: (data, elem) => {
-      console.log(data);
-      console.log(elem);
-      selectedDate = data;
-      alert(`Vous avez sélectionné le ${new Date(selectedDate.date)}`);
+    console.log(data);
+    console.log(elem);
+    selectedDate = data;
   },
 });
 
 let selectedDate = {};
+
+const movieForm = document.querySelector("#movie-form");
+movieForm.addEventListener("submit", addMovie);
+
+function addMovie(e) {
+  e.preventDefault();
+  const formData = new FormData(movieForm);
+  console.log(formData);
+  const title = formData.get("title");
+  const year = formData.get("year");
+  const duration = formData.get("duration");
+  const genres = formData.get("genres");
+  console.log(title, year, duration, genres);
+}
