5720e649dbc9323ea2e240c251a008f21767a159
codeconcept <contact@codeconcept.fr>
2021-10-08 12:55:01 +0200

    Delete movie + redisplay movies by date

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const calendar = new VanillaCalendar({
   onSelect: (data, elem) => {
     console.log(data);
     console.log(elem);
-    selectedDate = data;
-    displayMovie(new Date(data.date).toISOString());
+    selectedDate.date = new Date(data.date).toISOString();
+    displayMoviesByDate(selectedDate.date);
   },
 });
 
@@ -29,6 +29,7 @@ const movieForm = document.querySelector("#movie-form");
 movieForm.addEventListener("submit", addMovie);
 
 const movieList = document.querySelector("#movie-list");
+movieList.addEventListener("click", deleteMovie);
 
 function addMovie(e) {
   e.preventDefault();
@@ -39,11 +40,12 @@ function addMovie(e) {
   const duration = formData.get("duration");
   const genres = formData.getAll("genres");
   const newMovie = {
+    id: Date.now(),
     title,
     year: Number(year),
     duration,
     genres,
-    date: new Date(selectedDate.date),
+    date: selectedDate.date,
   };
   console.log(newMovie);
   saveMovie(newMovie);
@@ -54,9 +56,10 @@ function saveMovie(movie) {
   let movies = JSON.parse(localStorage.getItem("movies")) || [];
   movies = [...movies, movie];
   localStorage.setItem("movies", JSON.stringify(movies));
+  displayMoviesByDate(selectedDate.date);
 }
 
-function displayMovie(date) {
+function displayMoviesByDate(date) {
   let movies = JSON.parse(localStorage.getItem("movies")) || [];
   const moviesAtThisDate = movies.filter((movie) => {
     // retrieve date without time
@@ -70,6 +73,7 @@ function displayMovie(date) {
         <span>année : ${movie.year}</span>
         <span>durée : ${movie.duration}</span>
         <span>genre : ${movie.genres.join(", ")}</span>
+        <button data-id="${movie.id}">suppr.</button>
         </div>
         `;
   } else if (moviesAtThisDate.length > 1) {
@@ -93,10 +97,22 @@ function displayMovies(movies) {
             <span>année : ${movie.year}</span>
             <span>durée : ${movie.duration}</span>
             <span>genre : ${movie.genres.join(", ")}</span>
+            <button data-id="${movie.id}">suppr.</button>
             </div>
             `;
     content = [...content, singleMovieHTML];
   });
 
   movieList.innerHTML = content.join("");
+}
+
+function deleteMovie(e) {
+  if (e.target.nodeName.toLowerCase() !== "button") {
+    return;
+  }
+  const movieId = Number(e.target.dataset.id);
+  let movies = JSON.parse(localStorage.getItem("movies")) || [];
+  movies = movies.filter((movie) => movie.id !== movieId);
+  localStorage.setItem("movies", JSON.stringify(movies));
+  displayMoviesByDate(selectedDate.date);
 }
